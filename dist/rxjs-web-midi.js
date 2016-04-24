ab32e26f1b6d8817e764f82d3540288c1cd7e196
Arto Chydenius <arto.chydenius@futurice.com>
2016-04-24 22:42:06 +0300

    Update tests to match ESLint config & do some small stylistic changes to lib

--- dist/rxjs-web-midi.js
+++ dist/rxjs-web-midi.js
@@ -1,8 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
-    value: true
+  value: true
 });
 exports.midimessageAsObservable = midimessageAsObservable;
 exports.statechangeAsObservable = statechangeAsObservable;
@@ -13,33 +15,33 @@ var _rx = require('rx');
 
 var _rx2 = _interopRequireDefault(_rx);
 
+var createObservable = function createObservable(object, handlerName) {
+  return _rx2['default'].Observable.create(function (observer) {
+    if (object.observers === undefined) {
+      object.observers = [];
+      object[handlerName] = function (event) {
+        object.observers.forEach(function (obs) {
+          obs.onNext(event);
+        });
+      };
+    }
+
+    object.observers.push(observer);
+
+    return _rx2['default'].Disposable.create(function () {
+      object.observers = object.observers.filter(function (x) {
+        return x !== observer;
+      });
+    });
+  });
+};
+
 function midimessageAsObservable(input) {
-    return createObservable(input, 'onmidimessage');
+  return createObservable(input, 'onmidimessage');
 }
 
 function statechangeAsObservable(midi) {
-    return createObservable(midi, 'onstatechange');
-}
-
-function createObservable(object, handlerName) {
-    return _rx2['default'].Observable.create(function (observer) {
-        if (object.observers === undefined) {
-            object.observers = [];
-            object[handlerName] = function (event) {
-                object.observers.forEach(function (observer) {
-                    observer.onNext(event);
-                });
-            };
-        }
-
-        object.observers.push(observer);
-
-        return _rx2['default'].Disposable.create(function () {
-            object.observers = object.observers.filter(function (x) {
-                return x !== observer;
-            });
-        });
-    });
+  return createObservable(midi, 'onstatechange');
 }
 
 },{"rx":3}],2:[function(require,module,exports){
--- test/test.js
+++ test/test.js
@@ -1,112 +1,108 @@
 import assert from 'assert';
-import { midimessageAsObservable, statechangeAsObservable } from '../lib/rxjs-web-midi'; 
+import { midimessageAsObservable, statechangeAsObservable } from '../lib/rxjs-web-midi';
 import Rx from 'rx';
 
-function createMidiAccessMock() {
-    return {
-        statechange: function(data) {
-            this.onstatechange(data);
-        }
-    };
+const createMidiAccessMock = () => ({
+  statechange: function stateChange(data) {
+    this.onstatechange(data);
+  },
+});
+
+const createMidiInputMock = () => ({
+  midimessage: function midiMessage(data) {
+    this.onmidimessage(data);
+  },
+});
+
+const testMultipleSubscribers = (mock, func, observable) => {
+  const scheduler = new Rx.TestScheduler();
+  const observers = [
+    scheduler.createObserver(),
+    scheduler.createObserver(),
+  ];
+
+  scheduler.scheduleAbsolute(0, () => {
+    observable.subscribe(observers[0]);
+    observable.subscribe(observers[1]);
+  });
+  scheduler.scheduleAbsolute(50, () => {
+    mock[func](1);
+  });
+  scheduler.scheduleAbsolute(100, () => {
+    mock[func](2);
+  });
+  scheduler.start();
+
+  const expected = [
+    Rx.ReactiveTest.onNext(50, 1),
+    Rx.ReactiveTest.onNext(100, 2),
+  ];
+
+  assert.deepEqual(expected, observers[0].messages);
+  assert.deepEqual(expected, observers[1].messages);
 };
 
-function createMidiInputMock() {
-    return {
-        midimessage: function(data) {
-            this.onmidimessage(data);
-        }
-    };
+const testDisposing = (mock, func, observable) => {
+  const scheduler = new Rx.TestScheduler();
+  const observers = [
+    scheduler.createObserver(),
+    scheduler.createObserver(),
+  ];
+  const subscriptions = [];
+
+  scheduler.scheduleAbsolute(0, () => {
+    subscriptions[0] = observable.subscribe(observers[0]);
+    subscriptions[1] = observable.subscribe(observers[1]);
+  });
+  scheduler.scheduleAbsolute(50, () => mock[func](1));
+  scheduler.scheduleAbsolute(100, () => subscriptions[0].dispose());
+  scheduler.scheduleAbsolute(150, () => mock[func](2));
+  scheduler.start();
+
+  const expectedFirst = [
+    Rx.ReactiveTest.onNext(50, 1),
+  ];
+  const expectedSecond = [
+    Rx.ReactiveTest.onNext(50, 1),
+    Rx.ReactiveTest.onNext(150, 2),
+  ];
+
+  assert.deepEqual(expectedFirst, observers[0].messages);
+  assert.deepEqual(expectedSecond, observers[1].messages);
 };
-
-function testMultipleSubscribers(mock, func, observable) {
-    const scheduler = new Rx.TestScheduler();
-    const observers = [
-        scheduler.createObserver(),
-        scheduler.createObserver()
-    ];
-
-    scheduler.scheduleAbsolute(0, () => {
-        observable.subscribe(observers[0]);
-        observable.subscribe(observers[1]);
-    });
-    scheduler.scheduleAbsolute(50, () => {
-        mock[func](1);
-    });
-    scheduler.scheduleAbsolute(100, () => {
-        mock[func](2);
-    });
-    scheduler.start();
-
-    const expected = [
-        Rx.ReactiveTest.onNext(50, 1),
-        Rx.ReactiveTest.onNext(100, 2)
-    ];
-
-    assert.deepEqual(expected, observers[0].messages);
-    assert.deepEqual(expected, observers[1].messages);
-}
-
-function testDisposing(mock, func, observable) {
-    const scheduler = new Rx.TestScheduler();
-    const observers = [
-        scheduler.createObserver(),
-        scheduler.createObserver()
-    ];
-    const subscriptions = [];
-
-    scheduler.scheduleAbsolute(0, () => {
-        subscriptions[0] = observable.subscribe(observers[0]);
-        subscriptions[1] = observable.subscribe(observers[1]);
-    });
-    scheduler.scheduleAbsolute(50, () => mock[func](1));
-    scheduler.scheduleAbsolute(100, () => subscriptions[0].dispose());
-    scheduler.scheduleAbsolute(150, () => mock[func](2));
-    scheduler.start();
-
-    const expectedFirst = [
-        Rx.ReactiveTest.onNext(50, 1)
-    ];
-    const expectedSecond = [
-        Rx.ReactiveTest.onNext(50, 1),
-        Rx.ReactiveTest.onNext(150, 2)
-    ];
-
-    assert.deepEqual(expectedFirst, observers[0].messages);
-    assert.deepEqual(expectedSecond, observers[1].messages);
-}
 
 describe('statechangeAsObservable', () => {
-    it('should return an Observable', () => {
-        assert(statechangeAsObservable(createMidiAccessMock()) instanceof Rx.Observable);
-    });
+  it('should return an Observable', () => {
+    assert(statechangeAsObservable(createMidiAccessMock()) instanceof Rx.Observable);
+  });
 
-    it('should send events to multiple subscribers', () => {
-        const midi = createMidiAccessMock();
-        const observable = statechangeAsObservable(midi);
-        testMultipleSubscribers(midi, 'statechange', observable);
-    });
+  it('should send events to multiple subscribers', () => {
+    const midi = createMidiAccessMock();
+    const observable = statechangeAsObservable(midi);
+    testMultipleSubscribers(midi, 'statechange', observable);
+  });
 
-    it('should stop sending events to disposed subscribers', () => {
-        const midi = createMidiAccessMock();
-        const observable = statechangeAsObservable(midi);
-        testDisposing(midi, 'statechange', observable);
-    });
+  it('should stop sending events to disposed subscribers', () => {
+    const midi = createMidiAccessMock();
+    const observable = statechangeAsObservable(midi);
+    testDisposing(midi, 'statechange', observable);
+  });
 });
 
 describe('midimessageAsObservable', () => {
-    it('should return an Observable', () => {
-        assert(midimessageAsObservable(createMidiInputMock()) instanceof Rx.Observable);
-    })
+  it('should return an Observable', () => {
+    assert(midimessageAsObservable(createMidiInputMock()) instanceof Rx.Observable);
+  });
 
-    it('should send messages to multiple subscribers', () => {
-        const input = createMidiInputMock();
-        const observable = midimessageAsObservable(input);
-        testMultipleSubscribers(input, 'midimessage', observable);
-    })
+  it('should send messages to multiple subscribers', () => {
+    const input = createMidiInputMock();
+    const observable = midimessageAsObservable(input);
+    testMultipleSubscribers(input, 'midimessage', observable);
+  });
 
-    it('should stop sending messages to disposed subscribers', () => {
-        const input = createMidiInputMock();
-        const observable = midimessageAsObservable(input);
-        testDisposing(input, 'midimessage', observable);
-    });
+  it('should stop sending messages to disposed subscribers', () => {
+    const input = createMidiInputMock();
+    const observable = midimessageAsObservable(input);
+    testDisposing(input, 'midimessage', observable);
+  });
 });
